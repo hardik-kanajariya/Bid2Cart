a5ea2f11c14646d167239c679182668fafcc520a
Hardik Kanajariya <71688178+hardikkanajariya@users.noreply.github.com>
2025-07-20 01:17:18 +0530

    Load environment variables

--- public/bot.php
+++ public/bot.php
@@ -22,13 +22,15 @@ require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
+// Load environment variables from the current directory 
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Connecting with database
 $server   = $_ENV['DB_HOST'] ?? 'localhost';
 $username = $_ENV['DB_USERNAME'] ?? 'root';
 $password = $_ENV['DB_PASSWORD'] ?? '';
 $database = $_ENV['DB_DATABASE'] ?? 'bid2cart';
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 $conn = mysqli_connect($server, $username, $password, $database);
 
 // SMTP Server settings
