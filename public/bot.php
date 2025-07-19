00657e13d1c0d86c98004e92d6d3d7f6c888a773
Hardik Kanajariya <71688178+hardikkanajariya@users.noreply.github.com>
2025-07-20 01:16:20 +0530

    path update

--- public/bot.php
+++ public/bot.php
@@ -27,7 +27,7 @@ $server   = $_ENV['DB_HOST'] ?? 'localhost';
 $username = $_ENV['DB_USERNAME'] ?? 'root';
 $password = $_ENV['DB_PASSWORD'] ?? '';
 $database = $_ENV['DB_DATABASE'] ?? 'bid2cart';
-$dotenv = Dotenv::createImmutable(__DIR__. '/..');
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $conn = mysqli_connect($server, $username, $password, $database);
 
